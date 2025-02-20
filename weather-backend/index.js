0e01b81c9659d537d956816c41d1a98fab72bea2
alex518123 <alexresende518@gmail.com>
2025-02-20 15:04:48 -0300

    Update index.js

--- weather-backend/index.js
+++ weather-backend/index.js
@@ -5,39 +5,23 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
-
-// Usando a porta fornecida pelo Render ou 3000 se estiver localmente
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-// Rota para a raiz (mensagem explicativa)
-app.get('/', (req, res) => {
-  res.send('API de clima funcionando. Use o endpoint /weather?city={nome-da-cidade} para obter o clima.');
-});
-
-// Endpoint para obter dados climáticos (alterado para /weather)
 app.get('/weather', async (req, res) => {
-  const { city } = req.query;  // Obtém a cidade da consulta
-  if (!city) {
-    return res.status(400).json({ error: 'Por favor, forneça o nome da cidade na query, por exemplo: /weather?city=Londres' });
-  }
-  
+  const { city } = req.query;
   const apiKey = process.env.WEATHER_API_KEY;
 
   try {
-    // Requisição para a API do clima
     const response = await axios.get(`https://api.weatherapi.com/v1/current.json`, {
       params: {
         key: apiKey,
         q: city
       }
     });
-
-    // Retorna os dados do clima
     res.json(response.data);
   } catch (error) {
-    // Caso ocorra algum erro na requisição
     res.status(500).json({ error: 'Erro ao buscar dados do clima' });
   }
 });
@@ -45,4 +29,3 @@ app.get('/weather', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-

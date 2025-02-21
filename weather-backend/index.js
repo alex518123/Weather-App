597d4bb342b7fb6adf88a5cf6cc63c9ff6c54a77
alex518123 <alexresende518@gmail.com>
2025-02-20 21:02:36 -0300

    Update index.js

--- weather-backend/index.js
+++ weather-backend/index.js
@@ -6,13 +6,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Servir arquivos estáticos da pasta 'weather-frontend'
-app.use(express.static("weather-frontend"));
-
-app.use(cors({ origin: "*" }));
+app.use(cors({ 
+  origin: "*" ,
+  optionsSuccessStatus: 200,
+}));
 
 // Rota para buscar o clima
-app.get("/api/weather", async (req, res) => {
+app.get("/api/weather", (req, res) => {
   const cityName = req.query.city;
   const API_KEY = process.env.WEATHER_API_KEY;
 
@@ -28,21 +28,18 @@ app.get("/api/weather", async (req, res) => {
 
   const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityName}&days=2`;
 
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      // Se a resposta não for ok (por exemplo, 404 ou 500)
-      return res.status(response.status).json({ error: "Failed to fetch weather data from external API" });
-    }
-
-    const data = await response.json();
-    res.json(data); // Retorna os dados de clima para o front-end
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao buscar dados do clima" });
-  }
+  // Usando fetch com .then/.catch
+  fetch(url)
+    .then(resObj => resObj.json()) // Faz a conversão da resposta para JSON
+    .then(data => {
+      res.json(data); // Retorna os dados para o front-end
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: "Erro ao buscar dados do clima" });
+    });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
